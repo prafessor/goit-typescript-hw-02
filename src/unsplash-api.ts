63e089e6c6f7fa4cb1c617086beeceb809f62674
prafessor <olegmilyo@gmail.com>
2025-04-30 12:49:33 +0300

    fix request type

--- src/unsplash-api.ts
+++ src/unsplash-api.ts
@@ -10,7 +10,7 @@ export default async function searchImage(
   queryImg: string,
   currentPage: number
 ): Promise<SearchImage> {
-  const response = await axios.get('https://api.unsplash.com/search/photos', {
+  const response = await axios.get<SearchImage>('https://api.unsplash.com/search/photos', {
     params: {
       client_id: 'nnKU6nPDzY3KzcUccSsntrotYQOV49QahVuopMsamZs',
       query: queryImg,
